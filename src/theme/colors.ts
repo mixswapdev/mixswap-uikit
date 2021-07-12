1663d2857c4d868b6526d82f4a82c0603bb21a7d
MXS Developer <dev@mixswap.finance>
2021-07-13 02:05:14 +0630

    updated base colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -23,9 +23,9 @@ export const lightColors: Colors = {
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#452A7A",
+  text: "#25c3e0",
   textDisabled: "#BDC2C4",
-  textSubtle: "#8f80ba",
+  textSubtle: "#015a6d",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
@@ -44,9 +44,9 @@ export const darkColors: Colors = {
   input: "#483f5a",
   primaryDark: "#0098A1",
   tertiary: "#353547",
-  text: "#EAE2FC",
+  text: "#25c3e0",
   textDisabled: "#666171",
-  textSubtle: "#c9c4d4",
+  textSubtle: "#22d6d1",
   borderColor: "#524B63",
   card: "#27262c",
   gradients: {
